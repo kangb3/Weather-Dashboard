99c0ad0756dbc18cf82c5b5bc78491f9537241b0
Baldeep Kang <bkang4612@gmail.com>
2020-07-23 13:34:37 -0700

    create function to retrieve five day forecast

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
 
-    function searchCity(city){
+    //Function for retrieving current weather for a city
+    //takes one argument, city representing city name
+    function currentWeather(city){
 
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b16b8dd85bc345406e0453d1f7fc5fe0";
         $.ajax({
@@ -8,7 +10,7 @@ $(document).ready(function(){
             method: "GET"
           }).then(function(response) {
 
-            console.log(response);
+            //console.log(response);
 
           // Constructing HTML containing city name and current weather info
             var cityName = $("<h1>").text(response.name);
@@ -17,13 +19,28 @@ $(document).ready(function(){
             var cityWindSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " mph");
 
             // empty content of card-body
-            $(".card-body").empty();
-            $(".card-body").append(cityName, cityTemp, cityHumidity, cityWindSpeed);
+            $(".currentWeather").empty();
+            $(".currentWeather").append(cityName, cityTemp, cityHumidity, cityWindSpeed);
 
           });
+    
+    
 
 
         
+    }
+
+    //Function for retrieving five day forecast for a city
+    //takes one argument, city representing user input for city
+    function forecast(city){
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=b16b8dd85bc345406e0453d1f7fc5fe0";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response){
+          console.log(response);
+        });
     }
 
     //Event handler when user clicks search button after input city name
@@ -35,7 +52,8 @@ $(document).ready(function(){
           var cityInput = $("#city-input").val().trim();
 
       // call searchCity function, passing cityInput as an argument
-          searchCity(cityInput);
+          currentWeather(cityInput);
+          forecast(cityInput);
 
       
 
