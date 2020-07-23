d116f306ccc4d7f27e3a918af50bd08202b77bcf
Baldeep Kang <bkang4612@gmail.com>
2020-07-23 03:28:28 -0700

    add display for current weather condition for city search

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     function searchCity(city){
 
-        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b16b8dd85bc345406e0453d1f7fc5fe0";
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=b16b8dd85bc345406e0453d1f7fc5fe0";
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -10,19 +10,35 @@ $(document).ready(function(){
 
             console.log(response);
 
+          // Constructing HTML containing city name and current weather info
+            var cityName = $("<h1>").text(response.name);
+            var cityTemp = $("<p>").text("Temperature: " + response.main.temp);
+            var cityHumidity = $("<p>").text("Humidity: " + response.main.humidity + "%");
+            var cityWindSpeed = $("<p>").text("Wind Speed: " + response.wind.speed + " mph");
+
+            // empty content of card-body
+            $(".card-body").empty();
+            $(".card-body").append(cityName, cityTemp, cityHumidity, cityWindSpeed);
+
           });
 
 
         
     }
 
-
+    //Event handler when user clicks search button after input city name
     $("#city-submit").on("click", function(event){
+      // Preventing the button from trying to submit the form
           event.preventDefault();
 
+      // Store the city name from search box
           var cityInput = $("#city-input").val().trim();
 
+      // call searchCity function, passing cityInput as an argument
           searchCity(cityInput);
+
+      
+
     });
 
-  });
+});
