3294f1774208677b26409764777232ea76effac6
Baldeep Kang <bkang4612@gmail.com>
2020-07-23 01:06:59 -0700

    add onclick function for search button

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-function searchCity(city){
+$(document).ready(function(){
 
-    var queryURL = "api.openweathermap.org/data/2.5/weather" + city + "q={city name}&appid=b16b8dd85bc345406e0453d1f7fc5fe0";
+    function searchCity(city){
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-      }).then(function(response) {
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b16b8dd85bc345406e0453d1f7fc5fe0";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+          }).then(function(response) {
+
+            console.log(response);
+
+          });
 
 
+        
+    }
 
-      });
 
-}
+    $("#city-submit").on("click", function(event){
+          event.preventDefault();
+
+          var cityInput = $("#city-input").val().trim();
+
+          searchCity(cityInput);
+    });
+
+  });
